4a4d113a366950a81f167989f046d0b3c482b590
jguadagno <jguadagno@hotmail.com>
2021-03-24 19:26:37 -0700

    first commit

---
+++ autocomplete.js
@@ -0,0 +1,75 @@
+let map;
+let azureSearchDataSource;
+let azureMapDataSource;
+const mapCenter = [-73.985130, 40.758896]
+const defaultZoom = 15;
+
+function initializeMap() {
+    map = new atlas.Map('mapControl', {
+        center: mapCenter,
+        zoom: defaultZoom,
+        authOptions: {
+            authType: "subscriptionKey",
+            subscriptionKey: azureKey
+        }
+    });
+
+    map.events.add('ready', function () {
+        azureMapDataSource = new atlas.source.DataSource();
+        map.sources.add(azureMapDataSource);
+        map.layers.add(new atlas.layer.SymbolLayer(azureMapDataSource));
+        azureMapDataSource.add(new atlas.data.Point(mapCenter));
+    })
+}
+
+$(() => {
+
+    azureSearchDataSource = new kendo.data.DataSource({
+        serverFiltering: true,
+        transport: {
+            read: {
+                url: "https://atlas.microsoft.com/search/poi/json?typeahead=true&api-version=1&view=Auto&language=en-US&countrySet=US&subscription-key=" + azureKey,
+                type: "get",
+                dataType: "json",
+                data: function() {
+                    var center = map.getCamera().center;
+                    var searchTerm = $("#queryText").data("kendoAutoComplete").value();
+                    return {
+                        lon: center[0],
+                        lat: center[1],
+                        query: searchTerm
+                    }
+                }
+            }
+        },
+        schema: {
+            type: "json",
+            data: function(response) {
+                return response.results;
+            },
+            model: {
+               id: "id"
+            }
+        }
+    })
+
+    $('#queryText').kendoAutoComplete({
+        minLength: 3,
+        placeHolder: "Select a venue",
+        dataValueField: "id",
+        dataTextField: "poi.name",
+        template: $('#autoCompleteItemTemplate').html(),
+        dataSource: azureSearchDataSource,
+        select: function (e) {
+            var item = e.dataItem;
+            console.log(item.poi.name);
+            
+            map.setCamera({
+                center: [item.position.lon, item.position.lat],
+                zoom: defaultZoom});
+            azureMapDataSource.add(new atlas.data.Point([item.position.lon, item.position.lat]));
+        }
+    });
+    initializeMap();
+
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Sample Code for Blog Post
+
+This is the sample code for the [Using Kendo UI Autocomplete Widget with Azure Maps Search service]() blog post.
+
+For this sample to work, you will need to 
+
+* Get a [Azure Maps Client id](https://docs.microsoft.com/en-us/azure/azure-maps/quick-demo-map-app#get-the-primary-key-for-your-account).
+* Create a file called `azurekey.js`
+    * Add the following code to file
+    * Replace the `<replace with your key>` with your Azure Maps Client id.
+
+```javascript
+const azureKey = "<replace with your key";
+```
